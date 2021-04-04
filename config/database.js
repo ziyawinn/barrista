46db43b6f4673d855d0918526bb46d3ba61c3c41
ziyawinn <ziyawinn@gmail.com>
2021-04-03 22:35:14 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'url' : "mongodb+srv://Zwinn99:Winnstorm@cluster1.6xzhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    'dbName': 'database'
+};
+
+/*
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
+*/
